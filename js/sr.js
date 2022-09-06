477730488643d83d265ab0c0f7ce2e622af1e639
crohnaro <viniciosalfa@hotmail.com>
2022-09-06 13:21:11 -0300

    Deploy versão 1

---
+++ js/sr.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({reset : true})
+
+sr.reveal('#main-section-texts',{ duration : 1000 })
+sr.reveal('#main-section-image',{ duration : 2500 })
+
+sr.reveal('#description-image', {duration :2000})
+sr.reveal('#description-texts', {duration :1500})
+
+sr.reveal('#main-text', {duration : 3000})
+
+sr.reveal('#course-description-image',{duration : 3000})
+sr.reveal('#course-container', {duration: 3500})
